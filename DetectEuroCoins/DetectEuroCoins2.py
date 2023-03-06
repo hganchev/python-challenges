11b5f953db24ff74deba736f15ab98c4a9221c63
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2023-03-06 10:27:08 +0200

    better loop logic

--- DetectEuroCoins/DetectEuroCoins2.py
+++ DetectEuroCoins/DetectEuroCoins2.py
@@ -3,6 +3,7 @@ import numpy as np
 
 # Load the image
 img = cv2.imread('./euros2_quarter.bmp')
+# img = cv2.imread('./euros4_ching.bmp')
 
 # Convert the image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -48,42 +49,36 @@ circles = cv2.HoughCircles(masked_image, cv2.HOUGH_GRADIENT, dp=1, minDist=15, p
 
 # Sort circles by size
 circles = np.round(circles[0, :]).astype("int")
-circles = sorted(circles, key=lambda x: x[2], reverse=True)
-print(str(circles))
+# circles = sorted(circles, key=lambda x: x[2], reverse=True)
+# print(str(circles))
 
 # Define coin sizes
-euro_sizes = {1: (np.arange(18.25 - 1, 18.25 + 1), 1, (80,127,255)), 
-              2: (np.arange(25.75 - 1, 25.75 + 1), 2, (0,140,255)), 
-              5: (np.arange(23.25 - 1, 23.25 + 1), 5, (220,20,60)), 
-              10: (np.arange(19.75 - 1, 19.75 + 1), 10, (0,215,255)), 
-              20: (np.arange(22.25 - 1, 22.25 + 1), 20, (20,105,139)), 
-              50: (np.arange(24.25 - 1, 24.25 + 1), 50, (255,191,0)), 
-              100: (np.arange(30.25 - 1, 30.25 + 1), 100, (139,61,72)), 
-              200: (np.arange(25.75 - 1, 25.75 + 1), 200, (10,30,255))}
+euro_sizes = {
+              1: ([16.25 - 0, 16.25 + 1], 1, (80,127,255)), 
+              2: ([18.75 - 1.2, 18.75 + 1], 2, (0,140,255)), 
+              5: ([21.25 - 0, 21.25 + 1.5], 5, (220,20,60)), 
+              10: ([19.75 - 0.5, 19.75 + 1], 10, (0,215,255)), 
+              20: ([22.25 - 1, 22.25 + 1], 20, (20,105,139)), 
+              50: ([24.25 - -0.5, 24.25 + 1], 50, (255,191,0)), 
+              100: ([23.25 - 2, 23.25 + 1], 100, (139,61,72)), 
+              200: ([25.75 - 0.5, 25.75 + 2], 200, (10,30,255))
+              }
 
-# Classify each coin
-values = []
-colors = []
+# Display the sorted coins with their values
 for c in circles:
     # Determine the size of the coin
     r = c[2]
     diam = round(r * 2) * 0.43
     for i in euro_sizes.keys():
-        if diam not in euro_sizes[i][0]:
+        if diam < min(euro_sizes[i][0]) or diam > max(euro_sizes[i][0]):
+            # cv2.circle(img, (c[0], c[1]), c[2], (0, 255, 0), 2)
             continue
         # Determine the value of the coin based on its size
         value = euro_sizes[i][1]
-        values.append(value)
-        color = euro_sizes[i][2]
-        colors.append(color)
-
-# Display the sorted coins with their values
-for i, c in enumerate(circles):
-    if i in range(len(values)):   
-        cv2.circle(img, (c[0], c[1]), c[2], colors[i], 2)
-        cv2.putText(img, str(values[i]), (c[0]-20, c[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    else:
-        cv2.circle(img, (c[0], c[1]), c[2], (0, 255, 0), 2)
+        color = euro_sizes[i][2]   
+        cv2.circle(img, (c[0], c[1]), c[2], color, 2)
+        cv2.putText(img, str(value), (c[0]-20, c[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        
 
 cv2.imshow('Coins', img)
 cv2.waitKey(0)
