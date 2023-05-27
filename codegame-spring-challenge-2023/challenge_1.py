5820cfedcc22301d252f520cb31625b2117aa598
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2023-05-27 11:13:26 +0300

    solution 1 - make lines to all the cells with recources

--- codegame-spring-challenge-2023/challenge_1.py
+++ codegame-spring-challenge-2023/challenge_1.py
@@ -4,18 +4,27 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
+cellsToLineNumbers = [] # cells that formes the line to cell with recource
 number_of_cells = int(input())  # amount of hexagonal cells in this map
+print("number_of_cells: ", number_of_cells, file=sys.stderr, flush=True)
 for i in range(number_of_cells):
     # _type: 0 for empty, 1 for eggs, 2 for crystal
     # initial_resources: the initial amount of eggs/crystals on this cell
     # neigh_0: the index of the neighbouring cell for each direction
     _type, initial_resources, neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5 = [int(j) for j in input().split()]
+    if _type > 0:
+        cellsToLineNumbers.append(i)
 
 number_of_bases = int(input())
 for i in input().split():
     my_base_index = int(i)
 for i in input().split():
     opp_base_index = int(i)
+
+# Do the actions
+actions = []
+for cell in cellsToLineNumbers:
+    actions.append("LINE " + str(my_base_index) + " " + str(cell) + " " + str(5))
 
 # game loop
 while True:
@@ -31,7 +40,7 @@ while True:
     print("my_ants: ", my_ants, file=sys.stderr, flush=True)
     print("opp_ants: ", opp_ants, file=sys.stderr, flush=True)
     # WAIT | LINE <sourceIdx> <targetIdx> <strength> | BEACON <cellIdx> <strength> | MESSAGE <text>
-    actions = []
+    print("actions: ", actions, file=sys.stderr, flush=True)
 
     # TODO: choose actions to perform and push them into actions
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
