df3c037fdf290ab5d67f2845851bdbf3f256f64e
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2023-10-08 17:45:30 +0300

    ++

--- festo-coding-challange-2023/Chapter-1/Story.py
+++ festo-coding-challange-2023/Chapter-1/Story.py
@@ -25,3 +25,28 @@
 
 # Cosmo tilts its head slightly, "Well, next, we need to gain access to the inside of the temple building. The entrance is over there. Again, it opens with a 4-digit code on the access panel. The cipher matrix is the same as before, but we need to find new cipher plates. I sense some weak signals. Let me guide you!"
 
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+
+
+# Read the cipher matrix from the file cipher_matrix.png and add it to the plot.
+cifer_matrix_img = Image.open('cipher_matrix.png')
+
+
+# Read the cipher plates from the files plate_01.png, plate_02.png and plate_03.png and add them to the plot.
+plate_1_img = Image.open('plate_11.png')
+# plate_2_img = Image.open('plate_12.png')
+# plate_3_img = Image.open('plate_13.png')
+
+# Create a plot with 1 row and 1 column and show the cipher matrix and the cipher plates on top.
+cifer_matrix_img_array = np.array(cifer_matrix_img)
+plate_1_img_array = np.array(plate_1_img)
+# plate_2_img_array = np.array(plate_2_img)
+# plate_3_img_array = np.array(plate_3_img)
+
+imgplot = plt.imshow(cifer_matrix_img_array)
+imgplot = plt.imshow(plate_1_img_array)
+# imgplot = plt.imshow(plate_2_img_array)
+# imgplot = plt.imshow(plate_3_img_array)
+plt.show()
